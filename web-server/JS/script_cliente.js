d5050dc82d068750ce32c54a64932b5b903a861b
Axel Garcia <145340514+axelgarcia1A@users.noreply.github.com>
2025-05-05 15:26:53 +0200

    Delete web-server/JS directory

--- web-server/JS/script_cliente.js
+++
@@ -1,6 +0,0 @@
-        // Confirmación para cerrar sesión
-        document.querySelector('a[href="logout.php"]')?.addEventListener('click', function(e) {
-            if (!confirm('¿Estás seguro de que deseas cerrar sesión?')) {
-                e.preventDefault();
-            }
-        });
